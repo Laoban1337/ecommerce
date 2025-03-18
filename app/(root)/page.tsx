91b852c81b8f93859d8a1da2b8447376089ddacd
Brian Monestime <bmonestime@gmail.com>
2025-03-18 12:00:57 -0600

    Sample Products & ProductList

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,9 @@
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
+import ProductList from "@/components/ui/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const Homepage = () => {
-  return <Button>Prostore</Button>;
+  console.log(sampleData)
+  return <><ProductList data={sampleData.products} title='Newest Arrivals' limit={4}/></>;
 };
 export default Homepage;
